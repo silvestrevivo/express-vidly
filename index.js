2a9f93be730c67eb28cbe60cf22047c65333da96
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-25 00:37:12 +0200

    comments and explanations

--- index.js
+++ index.js
@@ -11,13 +11,25 @@ const genres = [
   { id: 3, name: 'Romance' },
 ];
 
+// GET requests
 app.get('/api/genres', (req, res) => {
   res.send(genres);
 });
 
+app.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+
+  // 404 => object not found, not exists in the server
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  res.send(genre);
+});
+
+// POST requests
 app.post('/api/genres', (req, res) => {
+  // Validation
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+  // in post request, 400 means a BAD request
 
   const genre = {
     id: genres.length + 1,
@@ -27,33 +39,40 @@ app.post('/api/genres', (req, res) => {
   res.send(genre);
 });
 
+// PUT requests
 app.put('/api/genres/:id', (req, res) => {
+  // Try to find the genre to update
   const genre = genres.find(c => c.id === parseInt(req.params.id));
+
+  // 404 => object not found, not exists in the server
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
+  // Validation
   const { error } = validateGenre(req.body);
+  // 400 => on put is a Bad request
   if (error) return res.status(400).send(error.details[0].message);
 
+  // Update genre
   genre.name = req.body.name;
   res.send(genre);
 });
 
+// DELETE requests
 app.delete('/api/genres/:id', (req, res) => {
+  // Try to find the genre to delete
   const genre = genres.find(c => c.id === parseInt(req.params.id));
+
+  // 404 => object not found, not exists in the server
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
+  // Element to delete
   const index = genres.indexOf(genre);
   genres.splice(index, 1);
 
   res.send(genre);
 });
 
-app.get('/api/genres/:id', (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  res.send(genre);
-});
-
+// Function validation
 function validateGenre(genre) {
   const schema = {
     name: Joi.string().min(3).required()
